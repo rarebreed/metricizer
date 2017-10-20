1ea3c6b36175ef9f0592e1692c100ce0ac9ca867
Sean Toner <stoner@redhat.com>
2017-10-20 06:54:39 -0400

    Adding app.js

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@
  * 
  */
 const m = require("./metrics")
+
+m.main({major: 7, variant: "Server", arch: "x86_64"},
+       {tab: "QE-RHEL7.5", job: "rhsm-rhel-7.5-AllDistros-Tier1Tests", build: 13, pw: "334c628e5e5df90ae0fabb77db275c54"})
